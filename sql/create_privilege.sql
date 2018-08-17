713faee852d82d7c3446d8603304ae24deeb922e
王耀 <44947737@qq.com>
2018-08-17 14:17:13 +0800

    版本1 1.不带Redis缓存 2.使用原始的DataSource 3.只有初使的权限类

--- sql/create_privilege.sql
+++ sql/create_privilege.sql
@@ -28,7 +28,7 @@ COMMENT='系统模块表'
 
 INSERT INTO `p_module`(id,name,flag,url,sort_no,create_person,create_date,update_person,update_date) 
 VALUES
-(1,'配置中心','p','http://127.0.0.1:10002/privilege-server',10,'system',NOW(),'system',NOW())
+(1,'配置中心','p','http://127.0.0.1:8080/riversys',10,'system',NOW(),'system',NOW())
 ;
 
 
@@ -84,9 +84,9 @@ INDEX `idx_username` (`username`) USING BTREE
 COMMENT='用户表'
 ;
 
-INSERT INTO `p_user` VALUES (1, 'root', '63a9f0ea7bb98050796b649e85481845', '管理员', 1, 1, 0, 1, 0, 'system', '2013-10-21 01:04:49', 'system', '2013-10-21 01:04:49');
-INSERT INTO `p_user` VALUES (2, 'cl', '63a9f0ea7bb98050796b649e85481845', '创力电子商务', 1, 0, 1, 2, 0, 'root', '2013-10-23 01:09:30', 'root', '2013-10-23 01:09:30');
-INSERT INTO `p_user` VALUES (3, 'test', '63a9f0ea7bb98050796b649e85481845', '创力测试', 1, 0, 2, 3, 1, 'root', '2013-10-21 01:06:08', 'root', '2013-10-21 01:06:08');
+INSERT INTO `p_user` VALUES (1, 'root', '63a9f0ea7bb98050796b649e85481845', '管理员', 1, 1, 0, 1, 0, 'system', '2018-10-21 01:04:49', 'system', '2018-10-21 01:04:49');
+INSERT INTO `p_user` VALUES (2, 'cl', '63a9f0ea7bb98050796b649e85481845', '创力电子商务', 1, 0, 1, 2, 0, 'root', '2018-10-23 01:09:30', 'root', '2018-10-23 01:09:30');
+INSERT INTO `p_user` VALUES (3, 'test', '63a9f0ea7bb98050796b649e85481845', '创力测试', 1, 0, 2, 3, 1, 'root', '2018-10-21 01:06:08', 'root', '2018-10-21 01:06:08');
 
 
 #角色表
@@ -126,9 +126,9 @@ INDEX `idx_resource_id` (`resource_id`) USING BTREE
 COMMENT='角色权限资源关系表'
 ;
 
-INSERT INTO `p_role` VALUES (1, '物流总监', '物流总监', 'root', '2013-10-23 01:08:24', 'root', '2013-10-23 01:08:43');
-INSERT INTO `p_role` VALUES (2, '客服总监', '客服总监', 'root', '2013-10-23 01:08:32', 'root', '2013-10-23 01:08:38');
-INSERT INTO `p_role` VALUES (3, '运营总监', '运营总监', 'root', '2013-10-21 01:05:31', 'root', '2013-10-21 01:05:34');
+INSERT INTO `p_role` VALUES (1, '物流总监', '物流总监', 'root', '2018-10-23 01:08:24', 'root', '2018-10-23 01:08:43');
+INSERT INTO `p_role` VALUES (2, '客服总监', '客服总监', 'root', '2018-10-23 01:08:32', 'root', '2018-10-23 01:08:38');
+INSERT INTO `p_role` VALUES (3, '运营总监', '运营总监', 'root', '2018-10-21 01:05:31', 'root', '2018-10-21 01:05:34');
 
 INSERT INTO `p_role_module` VALUES (1, 1);
 INSERT INTO `p_role_module` VALUES (2, 1);
@@ -172,16 +172,16 @@ PRIMARY KEY (`id`)
 COMMENT='部门表'
 ;
 
-INSERT INTO `p_department` VALUES (1, '创力电子商务', '', 0, 's-1', '1', 'system', '2013-10-21 01:04:50', 'root', '2013-10-23 01:03:39');
-INSERT INTO `p_department` VALUES (2, '商品中心', '', 1, 's-1-1', '2', 'root', '2013-10-23 00:55:56', 'root', '2013-10-23 01:05:13');
-INSERT INTO `p_department` VALUES (3, '技术中心', '', 1, 's-1-2', '3', 'root', '2013-10-23 00:56:10', 'root', '2013-10-23 01:06:18');
-INSERT INTO `p_department` VALUES (4, '管理中心', '', 1, 's-1-3', '1', 'root', '2013-10-23 00:56:22', 'root', '2013-10-23 01:04:10');
-INSERT INTO `p_department` VALUES (5, '总裁办', '', 4, 's-1-3-1', '1', 'root', '2013-10-23 00:56:41', 'root', '2013-10-23 01:04:23');
-INSERT INTO `p_department` VALUES (6, '财务部', '', 4, 's-1-3-2', '2', 'root', '2013-10-23 01:03:56', 'root', '2013-10-23 01:04:32');
-INSERT INTO `p_department` VALUES (7, '人力资源部', '', 4, 's-1-3-3', '3', 'root', '2013-10-23 01:04:51', 'root', '2013-10-23 01:04:51');
-INSERT INTO `p_department` VALUES (8, '开发组', '', 3, 's-1-2-1', '1', 'root', '2013-10-23 01:06:02', 'root', '2013-10-23 01:06:57');
-INSERT INTO `p_department` VALUES (9, '测试组', '', 3, 's-1-2-2', '2', 'root', '2013-10-23 01:06:34', 'root', '2013-10-23 01:07:04');
-INSERT INTO `p_department` VALUES (10, '运维组', '', 3, 's-1-2-3', '3', 'root', '2013-10-23 01:07:12', 'root', '2013-10-23 01:07:12');
-INSERT INTO `p_department` VALUES (11, '视觉形象部', '', 2, 's-1-1-3', '3', 'root', '2013-10-23 01:05:47', 'root', '2013-10-23 01:05:47');
-INSERT INTO `p_department` VALUES (12, '女装部', '', 11, 's-1-1-1', '1', 'root', '2013-10-23 01:05:23', 'root', '2013-10-23 01:05:23');
-INSERT INTO `p_department` VALUES (13, '男装部', '', 11, 's-1-1-2', '2', 'root', '2013-10-23 01:05:32', 'root', '2013-10-23 01:05:32');
+INSERT INTO `p_department` VALUES (1, '创力电子商务', '', 0, 's-1', '1', 'system', '2018-10-21 01:04:50', 'root', '2018-10-23 01:03:39');
+INSERT INTO `p_department` VALUES (2, '商品中心', '', 1, 's-1-1', '2', 'root', '2018-10-23 00:55:56', 'root', '2018-10-23 01:05:13');
+INSERT INTO `p_department` VALUES (3, '技术中心', '', 1, 's-1-2', '3', 'root', '2018-10-23 00:56:10', 'root', '2018-10-23 01:06:18');
+INSERT INTO `p_department` VALUES (4, '管理中心', '', 1, 's-1-3', '1', 'root', '2018-10-23 00:56:22', 'root', '2018-10-23 01:04:10');
+INSERT INTO `p_department` VALUES (5, '总裁办', '', 4, 's-1-3-1', '1', 'root', '2018-10-23 00:56:41', 'root', '2018-10-23 01:04:23');
+INSERT INTO `p_department` VALUES (6, '财务部', '', 4, 's-1-3-2', '2', 'root', '2018-10-23 01:03:56', 'root', '2018-10-23 01:04:32');
+INSERT INTO `p_department` VALUES (7, '人力资源部', '', 4, 's-1-3-3', '3', 'root', '2018-10-23 01:04:51', 'root', '2018-10-23 01:04:51');
+INSERT INTO `p_department` VALUES (8, '开发组', '', 3, 's-1-2-1', '1', 'root', '2018-10-23 01:06:02', 'root', '2018-10-23 01:06:57');
+INSERT INTO `p_department` VALUES (9, '测试组', '', 3, 's-1-2-2', '2', 'root', '2018-10-23 01:06:34', 'root', '2018-10-23 01:07:04');
+INSERT INTO `p_department` VALUES (10, '运维组', '', 3, 's-1-2-3', '3', 'root', '2018-10-23 01:07:12', 'root', '2018-10-23 01:07:12');
+INSERT INTO `p_department` VALUES (11, '视觉形象部', '', 2, 's-1-1-3', '3', 'root', '2018-10-23 01:05:47', 'root', '2018-10-23 01:05:47');
+INSERT INTO `p_department` VALUES (12, '女装部', '', 11, 's-1-1-1', '1', 'root', '2018-10-23 01:05:23', 'root', '2018-10-23 01:05:23');
+INSERT INTO `p_department` VALUES (13, '男装部', '', 11, 's-1-1-2', '2', 'root', '2018-10-23 01:05:32', 'root', '2018-10-23 01:05:32');
